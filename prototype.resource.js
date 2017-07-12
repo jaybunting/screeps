2e7dae389c51b3922dbd113f45d63ea2241bc914
Jason Bunting <jbunting@daloran.com>
2017-07-11 22:59:21 -0700

    .

--- prototype.resource.js
+++ prototype.resource.js
@@ -1,7 +1,7 @@
 module.exports = function() {
     Resource.prototype.gatherEnergy =
         function(creep) {
-            if (creep.harvest(this) == ERR_NOT_IN_RANGE) {
+            if (creep.pickup(this) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(this, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         };
