61b9a138b894e3e4d0f0c9968e350a9d55496fc5
Jason Bunting <jbunting@daloran.com>
2017-07-12 00:54:35 -0700

    .

--- prototype.container.js
+++ prototype.container.js
@@ -4,5 +4,9 @@ module.exports = function() {
             if (creep.withdraw(this, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(this, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
+            if (this.store[RESOURCE_ENERGY] < 10) {
+                creep.memory.source = '';
+                creep.memory.upgrading = true;
+            }
         };
 };
