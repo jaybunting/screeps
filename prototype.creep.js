50862f66fba9088a036b34f367bddf7d9f52493a
Jason Bunting <jbunting@daloran.com>
2017-07-18 22:22:31 -0700

    Removed harvester role

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,16 +1,3 @@
-var roles = {
-    upgrader: require('role.upgrader'),
-    miner: require('role.miner'),
-    builder: require('role.builder'),
-    scavanger: require('role.scavanger'),
-    repairer: require('role.repairer'),
-    transport: require('role.transport'),
-    longDistanceHarvester: require('role.longdistanceharvester'),
-    attacker: require('role.attacker'),
-    claimer: require('role.claimer'),
-    envoy: require('role.envoy')
-};
-
 module.exports = function() {
     Creep.prototype.runRole =
         function() {
