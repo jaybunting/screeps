0e2a69c4ad6d98f20d87b2b7386195bb29e6660b
Jason Bunting <jbunting@daloran.com>
2017-08-03 22:45:05 -0700

    .

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ module.exports.loop = function () {
     
     for (var eachroom in roomlist) {
         console.log("Doing stuff for room: " + roomlist[eachroom]);
+        Game.rooms[roomlist[eachroom]].memory.spawn = Game.rooms[roomlist[eachroom]].find(FIND_STRUCTURES, { filter: (structure) => { return (structure.structureType == STRUCTURE_SPAWN);}});
         Game.rooms[roomlist[eachroom]].memory.storage = Game.rooms[roomlist[eachroom]].find(FIND_STRUCTURES, { filter: (structure) => { return (structure.structureType == STRUCTURE_STORAGE);}});
 
         var roomspawn = Game.getObjectById(Game.rooms[roomlist[eachroom]].memory.spawn);
